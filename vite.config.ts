57387ad67e04b7851e318ab982121f2e00da13ca
Warren Hansen <warren@billow.co.za>
2025-01-29 10:18:41 +0200

    wip: github pages setup

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import vue from "@vitejs/plugin-vue";
 import ImportProps from 'unplugin-vue-import-props/vite';
 import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/vuema/',  // Add this line for GitHub Pages
   plugins: [
