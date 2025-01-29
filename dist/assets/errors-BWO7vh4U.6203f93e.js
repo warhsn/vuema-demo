c28cb4ed8fd7b3ec902dd8ddc2625dfeaab7f29a
Warren Hansen <warren@billow.co.za>
2025-01-29 10:39:47 +0200

    wip: github pages setup

---
+++ dist/assets/errors-BWO7vh4U.6203f93e.js
@@ -0,0 +1,1 @@
+import{i as o}from"./index.d68be814.js";const e=r=>o(()=>r.error!==void 0&&r.error!==null),i=r=>o(()=>e(r)?r.error:"");export{e,i as t};
---
+++ dist/assets/installation.87040e72.js
@@ -0,0 +1,10 @@
+import{d as s,c as o,a as i,b as t,e as n,r,o as l}from"./index.d68be814.js";const p={class:"content"},u=`import { createApp } from 'vue'
+import VueBulma from '@warhsn/vuema'
+import '@warhsn/vuema/style.css'
+
+const app = createApp(App)
+app.use(VueBulma)`,c=`# Using npm
+npm install @warhsn/vuema
+
+# Using yarn
+yarn add @warhsn/vuema`,g=s({__name:"installation",setup(m){return(d,e)=>{const a=r("highlightjs");return l(),o("div",p,[e[0]||(e[0]=i('<h1>Quick Start</h1><hr><p> A lightweight and powerful UI component library that seamlessly integrates Vue 3 with Bulma CSS framework. Built with TypeScript and carefully crafted to provide a simple yet robust solution for building modern user interfaces. </p><p> Vuema bridges the gap between Vue 3&#39;s reactive framework and Bulma&#39;s flexible CSS components. By providing a set of pre-built, type-safe components, it enables developers to rapidly build responsive and maintainable applications without sacrificing customization options or performance. </p><article class="message is-warning"><div class="message-body"> Ensure that Bulma has been installed into your application and is at version 1.0 or greater. If not simply run <code>npm i bulma</code>. </div></article><h3>Requirements</h3><ul><li>Vue.js 3.0 or greater</li><li>Bulma 1.0 or greater</li></ul><h3>Installation</h3>',8)),t(a,{class:"is-rounded",language:"shell",code:c}),e[1]||(e[1]=n("p",{class:"mt-3"},"Import the package into your vue application.",-1)),t(a,{class:"is-rounded",language:"javascript",code:u})])}}});export{g as default};
